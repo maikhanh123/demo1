3f5cae35c33e06c3d0082b98854172a255c15b88
maikhanh123 <maikhanh123@gmail.com>
2017-10-31 18:34:01 +0700

    demo1-news

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
     res.render("conten.ejs")
 });
+app.get("/news", function (req, res) {
+    res.render("news.ejs")
+});
 
 app.listen(2100, function () {
     console.log("server start !!! http://localhost:2100/ " );
